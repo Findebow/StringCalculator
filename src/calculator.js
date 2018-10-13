e4d150c47309a033f0fe3c29516b0a2322122622
Findebow <finnbogih17@ru.is>
2018-10-13 02:39:59 +0000

    Implemented if only one number return number

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,11 @@
 //calculator
 function add (number) {
-    return 0;
+    if(number == "") {
+        return 0;
+    }
+    
+
+    return parseInt(number);
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -5,3 +5,6 @@ it("should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
 
+it("should return number when there is only one number", () => {
+    expect(add("5")).toBe(5);
+});
