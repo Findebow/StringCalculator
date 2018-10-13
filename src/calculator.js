ffa051d6fc02c0dcdd6a25bdb3ae1e36ae468c07
Findebow <finnbogih17@ru.is>
2018-10-13 03:12:35 +0000

    Implemented sum of multiple numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,23 @@
 //calculator
-function add (number) {
-    if(number == "") {
+function add (numbers) {
+    if(numbers == "") {
         return 0;
     }
-    
-    if(number.includes(",")) {
-        var numberArray = number.split(",")
-
-        return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+    else if(numbers.includes(",")) {
+        var numberArray = numbers.split(",");
+        return sum(numberArray);
     }
     else {
-        return parseInt(number);
+        return parseInt(numbers);
     }
+}
+
+function sum (numberArray) {
+    var total = 0;
+    for(var i in numberArray) {
+        total += parseInt(numberArray[i]);
+    }
+    return total;
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -12,3 +12,7 @@ it("should return number when there is only one number", () => {
 it("should return the sum of two given numbers", () => {
     expect(add("1,2")).toBe(3);
 });
+
+it("should return the sum of multiple numbers", () => {
+    expect(add("1,2,3")).toBe(6);
+});
